153019416be5a3f08782b21c2f5d0b68e7670b87
AngelPerilla <estibenejjdkdkkdkd@gmail.com>
2024-09-02 16:26:59 -0500

    Se implementa correctamente plantilla AdminLTE

--- proyecto/proyecto/settings.py
+++ proyecto/proyecto/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-os.path.join(BASE_DIR, 'static'),
+os.path.join(BASE_DIR / 'static'),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
-from apl.views import *
+from apl.views import vista1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('apl/', include('apl.urls')),
+    path('apl/', include('apl.urls') ),
 ]
